e27e94d86e139ee0bfa32d980ef66f9fd5024275
anthony's xps <anthony.storti@uvm.edu>
2021-04-23 23:21:34 -0400

    added self addressing for server

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = socket.gethostbyname(socket.gethostname())
+        self.server = "165.227.184.113"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
