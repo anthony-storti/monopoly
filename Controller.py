c272cfd551985ac3abec0e05660262b7a230aec2
rstanzio <robert.stanzione@uvm.edu>
2021-05-02 23:47:24 -0400

    fixed jail buttons (i think)

--- Controller.py
+++ Controller.py
@@ -19,8 +19,8 @@ def roll_dice(player: Player, board: Board):
             roll1 = random.randint(1, 6)
             roll2 = random.randint(1, 6)
         else:
-            roll1 = int(input("roll 1"))
-            roll2 = int(input("roll 2"))
+            roll1 = int(input("roll 1: "))
+            roll2 = int(input("roll 2: "))
 
         player.roll_1 = roll1
         player.roll_2 = roll2
@@ -43,7 +43,7 @@ def roll_dice(player: Player, board: Board):
                     player.extra_turn = True
                 else:
                     player.location = 10
-                    player.jail_counter = 4
+                    player.jail_counter = 5
                     player.extra_turns = 0
                     player.x = 70
                     player.y = 770
@@ -158,7 +158,7 @@ def lands_on(tile: Tile, player: Player, comm_chest: List[CommunityChest], chanc
         note: the "in jail" functionality still has to be handled elsewhere
         '''
         player.in_jail = True
-        player.jail_counter = 3
+        player.jail_counter = 4
         player.location = 10
         player.x = 70
         player.y = 770
@@ -216,7 +216,7 @@ def lands_on(tile: Tile, player: Player, comm_chest: List[CommunityChest], chanc
         note: The "in jail" functionality is handled here
         '''
         if player.in_jail:
-            if player.jail_counter == 4:
+            if player.jail_counter > 3:
                 player.jail_counter -= 1
             if player.jail_counter > 0 or not player.rolled:
                 player.jail_counter -= 1
@@ -229,7 +229,9 @@ def lands_on(tile: Tile, player: Player, comm_chest: List[CommunityChest], chanc
                 for item in player.inventory:
                     if isinstance(item, (CommunityChest, Chance)):
                         if "Get out of Jail" in item.message:
+                            player.rolled = False
                             return [["Use card (required)", "jail_card_required"]]
+                player.rolled = False
                 return [["$50 bail (required)", "pay_bail_required"]]
         else:
             return ret
